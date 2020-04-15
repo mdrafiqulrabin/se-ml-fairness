3d547f4af9adec972bf2ddf5f4fc3f9187794a3c
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2020-04-15 15:45:57 -0500

    checkpoints for models

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ class Config:
         self.ORG_DATA_PATH = "dataset/preprocess"
         self.MM_ROWS_PATH  = "dataset/transform/rows_permutation"
         self.MM_COLS_PATH  = "dataset/transform/cols_permutation"
+        self.Checkpoint    = "model/checkpoint"
 
