394d43b747ff5aea70b9ce2271adf524377e2d5a
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2020-04-15 12:47:39 -0500

    row and col permutation

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+
+    def __init__(self):
+        self.ORG_DATA_PATH = "dataset/preprocess"
+        self.MM_ROWS_PATH  = "dataset/transform/rows_permutation"
+        self.MM_COLS_PATH  = "dataset/transform/cols_permutation"
+
