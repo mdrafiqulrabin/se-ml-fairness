16268c72beeae5dd223a1451778bf29facab16c3
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2020-04-15 23:12:48 -0500

    computing results on transformations

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ class Config:
         self.MM_ROWS_PATH  = "dataset/transform/rows_permutation"
         self.MM_COLS_PATH  = "dataset/transform/cols_permutation"
         self.Checkpoint    = "model/checkpoint"
-
+        self.RLT_TRA_ROWS  = "result/transform/rows_permutation"
+        self.RLT_TRA_COLS  = "result/transform/cols_permutation"
