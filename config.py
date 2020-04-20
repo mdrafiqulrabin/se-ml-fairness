263842d0f9e2fa3dc5ca5e58e69229421775ed5b
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2020-04-20 14:19:41 -0500

    Update config for RS

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 class Config:
 
     def __init__(self):
+        self.RS = 42
         self.ORG_DATA_PATH = "dataset/preprocess"
         self.MM_ROWS_PATH  = "dataset/transform/rows_permutation"
         self.MM_COLS_PATH  = "dataset/transform/cols_permutation"
